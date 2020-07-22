6d8b6f9819aedc9b4d081774d4c268429670a1ee
huiyu.eun <huiyu.eun@samsung.com>
2020-07-22 14:43:20 +0900

    Add sample codes
    Signed-off-by: huiyu.eun <huiyu.eun@samsung.com>

---
+++ src/NUIBrokerSample/NUIBrokerSample.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using Tizen.Applications;
+using Tizen.NUI;
+using Tizen.NUI.BaseComponents;
+
+namespace NUIBrokerSample
+{
+    class Program : NUIApplication
+    {
+        private ImageView testButton1;
+        private RoundBroker b1;
+        private FrameBrokerBase b2;
+
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            Initialize();
+        }
+
+        void Initialize()
+        {
+            Window.Instance.KeyEvent += OnKeyEvent;
+            Window.Instance.BackgroundColor = new Color(0.9f, 0.9f, 0.9f, 1.0f);
+            /*
+            TextLabel text = new TextLabel("Broker");
+            text.HorizontalAlignment = HorizontalAlignment.Center;
+            text.VerticalAlignment = VerticalAlignment.Center;
+            text.TextColor = Color.Black;
+            text.PointSize = 8.0f;
+            text.HeightResizePolicy = ResizePolicyType.FillToParent;
+            text.WidthResizePolicy = ResizePolicyType.FillToParent;
+            text.Position = new Position(0, -100);
+            Window.Instance.GetDefaultLayer().Add(text);
+            */
+            testButton1 = new ImageView()
+            {
+                //BackgroundColor = new Color(0.48f, 0.32f, 0.85f, 1.0f),
+                Size = new Size(100, 100),
+                Position = new Position(0, 100),
+                ParentOrigin = ParentOrigin.Center,
+                PivotPoint = PivotPoint.Center,
+                PositionUsesPivotPoint = true,
+                CornerRadius = 50.0f,
+        };
+            testButton1.SetImage(GetResourcePath() + "NUIBrokerSample.png");
+            /*
+            View testButton2 = new View()
+            {
+                BackgroundColor = Color.Blue,
+                Size = new Size(100, 50),
+                Position = new Position(0, 100),
+                ParentOrigin = ParentOrigin.Center,
+                PivotPoint = PivotPoint.Center,
+                PositionUsesPivotPoint = true
+            };
+            */
+            Window.Instance.Add(testButton1);
+            //Window.Instance.Add(testButton2);
+            testButton1.TouchEvent += TestButton1_TouchEvent;
+            //testButton2.TouchEvent += TestButton2_TouchEvent;
+
+            //b1 = new DefaultAnimationBroker(GetDefaultWindow());
+            b1 = new RoundBroker(GetDefaultWindow());
+            Window.Instance.SetFramerBroker(b1);
+            b1.sharedResource = testButton1;
+
+            //AppControl appControl = new AppControl();
+            //appControl.ApplicationId = "org.tizen.example.NUIProviderSample";
+            //Window.Instance.SendLaunchRequest(appControl, true);
+        }
+
+        public static string GetResourcePath()
+        {
+            return Tizen.Applications.Application.Current.DirectoryInfo.Resource;
+        }
+        private bool TestButton1_TouchEvent(object source, View.TouchEventArgs e)
+        {
+            Animation ani = new Animation(50);
+            if (e.Touch.GetState(0) == PointStateType.Down)
+            {
+                ani.AnimateTo(testButton1, "Scale", new Vector3(0.8f, 0.8f, 1.0f));
+            }
+            else if (e.Touch.GetState(0) == PointStateType.Up)
+            {
+                ani.AnimateTo(testButton1, "Scale", new Vector3(1.0f, 1.0f, 1.0f));
+                ani.Finished += Ani_Finished;
+            }
+            ani.Play();
+            return true;
+        }
+
+        private void Ani_Finished(object sender, EventArgs e)
+        {
+            AppControl appControl = new AppControl();
+            appControl.ApplicationId = "org.tizen.example.NUIProviderSample";
+            Window.Instance.SendLaunchRequest(appControl, true);
+        }
+
+
+        private bool TestButton2_TouchEvent(object source, View.TouchEventArgs e)
+        {
+            if (e.Touch.GetState(0) == PointStateType.Up)
+            {
+                Tizen.Log.Error("MYLOG", "Btn2 touch");
+                if (b1 != null)
+                {
+                    b1.Dispose();
+                    b1 = null;
+                }
+                if (b2 == null)
+                {
+                    b2 = new PositionBroker(GetDefaultWindow());
+                }
+                Window.Instance.SetFramerBroker(b2);
+
+                AppControl appControl = new AppControl();
+                appControl.ApplicationId = "org.tizen.example.NUIProviderSample";
+                Window.Instance.SendLaunchRequest(appControl, true);
+            }
+            return true;
+        }
+
+        public void OnKeyEvent(object sender, Window.KeyEventArgs e)
+        {
+            if (e.Key.State == Key.StateType.Down && (e.Key.KeyPressedName == "XF86Back" || e.Key.KeyPressedName == "Escape"))
+            {
+                Exit();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var app = new Program();
+            app.Run(args);
+        }
+    }
+}
---
+++ src/NUIBrokerSample/RoundBroker.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tizen.NUI;
+using Tizen.NUI.BaseComponents;
+
+namespace NUIBrokerSample
+{
+    class RoundBroker : FrameBrokerBase
+    {
+        private Window window;
+        private ImageView imgView;
+        public ImageView sharedResource;
+
+        
+        public RoundBroker(Window window) : base(window)
+        {
+            this.window = window;
+        }
+
+        protected override void OnFrameCreated()
+        {
+            base.OnFrameCreated();
+        }
+
+        protected override void OnFrameDestroyed()
+        {
+            base.OnFrameDestroyed();
+        }
+
+        protected override void OnFramePaused()
+        {
+            base.OnFramePaused();
+        }
+
+        protected override void OnFrameResumed(FrameData frame)
+        {
+            Tizen.Log.Error("MYLOG", "OnFrameResumed");
+            base.OnFrameResumed(frame);
+            imgView = frame.Image;
+            window.Add(imgView);
+            imgView.ParentOrigin = ParentOrigin.Center;
+            imgView.PivotPoint = PivotPoint.Center;
+            imgView.PositionUsesPivotPoint = true;
+            imgView.Size = new Size(360, 360);
+
+            Animation ani = new Animation(70);
+            ani.DefaultAlphaFunction = GetSineOut33();
+            ani.Finished += Ani_Finished;
+
+            Animation shared = new Animation(100);
+            shared.Finished += Shared_Finished;
+
+            if (frame.DirectionForward == true)
+            { 
+                Tizen.Log.Error("MYLOG", "***Forward animation");
+                imgView.Position = new Position(0, 100);
+                imgView.Scale = new Vector3(0.5f, 0.5f, 0.5f);
+
+                ani.AnimateTo(imgView, "Scale", new Vector3(1.0f, 1.0f, 1.0f));
+                ani.AnimateTo(imgView, "Position", new Position(0, 0));
+
+                shared.AnimateTo(sharedResource, "Position", new Position(0, -100));
+            }
+            else
+            {
+                Tizen.Log.Error("MYLOG", "***Backward animation");
+                imgView.Position = new Position(0, 0);
+                imgView.Scale = new Vector3(1.0f, 1.0f, 1.0f);
+
+                ani.AnimateTo(imgView, "Scale", new Vector3(0.5f, 0.5f, 0.5f));
+                ani.AnimateTo(imgView, "Position", new Position(0, 100));
+
+                shared.AnimateTo(sharedResource, "Position", new Position(0, 100));
+            }
+
+            ani.Play();
+            shared.Play();
+
+
+            Tizen.Log.Error("MYLOG", "Start Animation");
+            StartAnimation();
+        }
+
+        private void Shared_Finished(object sender, EventArgs e)
+        {
+            //sharedResource.Position = new Position(0, 100);
+        }
+
+        protected override void OnFrameUpdated(FrameData frame)
+        {
+            Tizen.Log.Error("MYLOG", "OnFrameUpdated");
+            base.OnFrameUpdated(frame);
+
+            //imgView.BackgroundColor = Color.White;
+        }
+
+        private void Ani_Finished(object sender, EventArgs e)
+        {
+            Tizen.Log.Error("MYLOG", "Finish Animation");
+            FinishAnimation();
+            imgView.Unparent();
+            imgView.Dispose();
+            //imgView = null;
+        }
+
+
+        /// <summary>
+        /// GlideOut, 0.25, 0.46, 0.45, 1.0
+        /// </summary>
+        internal AlphaFunction GetGlideOut()
+        {
+            return new AlphaFunction(new Vector2(0.25f, 0.46f), new Vector2(0.45f, 1.0f));
+        }
+
+        /// <summary>
+        /// SineInOut80, 0.33, 0.0, 0.2, 1.0
+        /// </summary>
+        internal AlphaFunction GetSineInOut80()
+        {
+            return new AlphaFunction(new Vector2(0.33f, 0.0f), new Vector2(0.2f, 1.0f));
+        }
+
+        /// <summary>
+        /// GetSineOut33, 0.17, 0.17, 0.67, 1.0
+        /// </summary>
+        internal AlphaFunction GetSineOut33()
+        {
+            return new AlphaFunction(new Vector2(0.17f, 0.17f), new Vector2(0.67f, 1.0f));
+        }
+
+        internal AlphaFunction GetEaseOutSquare()
+        {
+            return new AlphaFunction(AlphaFunction.BuiltinFunctions.EaseOutSquare);
+        }
+    }
+}
